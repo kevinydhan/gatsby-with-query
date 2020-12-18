15370bb302cc6ed2b5212c6c764d8da43efe4406
Kevin Han <kevinhanyd@gmail.com>
2020-12-18 00:07:55 -0500

    test: create tests for WithQueryHOC.displayName

--- src/__tests__/with-query.spec.ts
+++ src/__tests__/with-query.spec.ts
@@ -17,11 +17,6 @@ describe('withQuery object', () => {
   it('is a function', () => {
     expect(typeof withQuery).toBe('function')
   })
-
-  it('returns a function', () => {
-    const WithQueryHOC = withQuery(DivComponent, 1)
-    expect(typeof WithQueryHOC).toBe('function')
-  })
 })
 
 describe("withQuery's arguments", () => {
@@ -45,5 +40,40 @@ describe("withQuery's arguments", () => {
     expect(() => withQuery(DivComponent, [])).toThrowError()
     expect(() => withQuery(DivComponent, null)).toThrowError()
     expect(() => withQuery(DivComponent, undefined)).toThrowError()
+  })
+})
+
+describe("withQuery's return value", () => {
+  it('returns a function', () => {
+    const WithQueryHOC = withQuery(DivComponent, emptyQueryHook)
+    expect(typeof WithQueryHOC).toBe('function')
+  })
+})
+
+describe('WithQueryHOC.displayName', () => {
+  it('is defined', () => {
+    const WithQueryHOC = withQuery(DivComponent, emptyQueryHook)
+    expect(WithQueryHOC.displayName).toBeDefined()
+  })
+
+  it('is a string', () => {
+    const WithQueryHOC = withQuery(DivComponent, emptyQueryHook)
+    expect(typeof WithQueryHOC.displayName).toBe('string')
+  })
+
+  it('contains Component.displayName', () => {
+    const NameTestComponent = () => createElement('div')
+    const WithQueryHOC = withQuery(NameTestComponent, emptyQueryHook)
+    const containsName = WithQueryHOC.displayName.includes('NameTestComponent')
+    expect(containsName).toBe(true)
+  })
+
+  it('creates higher-order components with different displayName values', () => {
+    const NameTestComponent1 = () => createElement('div')
+    const NameTestComponent2 = () => createElement('div')
+    const WithQueryHOC1 = withQuery(NameTestComponent1, emptyQueryHook)
+    const WithQueryHOC2 = withQuery(NameTestComponent2, emptyQueryHook)
+
+    expect(WithQueryHOC1.displayName === WithQueryHOC2.displayName).toBe(false)
   })
 })
